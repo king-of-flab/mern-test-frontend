ec6837b20af0b5a244bbf0f682b4f710595d7584
king-of-flab <nawas@live.com.sg>
2017-08-28 13:36:47 +0800

    front end mern test

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+
+  componentDidMount () {
+    const url = 'https://quiet-crag-31264.herokuapp.com/articles'
+    console.log(url)
+
+    fetch(url)
+      .then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        console.log('Parsed JSON', json)
+      }).catch(function (ex) {
+        console.log('Parsing JSON failed', ex)
+      })
+  }
+
+}
+
+export default App;
